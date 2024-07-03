edbd3af5c477739abdeccb0ca60e671391019b6b
Prathamesh Chavan <88970197+prathamesh7120@users.noreply.github.com>
2024-07-03 18:57:21 +0530

    updated

---
+++ script.js
@@ -0,0 +1,22 @@
+const insert = document.getElementById('insert')
+
+window.addEventListener('keydown', (e) =>{
+    insert.innerHTML = `
+      <div class="color">
+        <table>
+  <tr>
+    <th>key</th>
+    <th>keycode</th>
+    <th>code</th>
+  </tr>
+  <tr>
+    <td>${e.key === " " ? "Space":e.key}s</td>
+    <td>${e.keyCode}</td>
+    <td>${e.code}</td>
+  </tr>
+
+</table>
+
+      </div>
+    `;
+})
